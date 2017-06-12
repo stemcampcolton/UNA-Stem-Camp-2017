eb58faa999cbca4f3178276d7768c60f6afe77e2
stemcampcolton <bandcgooch@gmail.com>
2017-06-12 11:21:35 -0500

    Initial commit

---
+++ hello-world/potato.c
@@ -0,0 +1,12 @@
+/*
+*Colton Gooch
+*Program #1
+*hello world
+*06/12/17
+*/
+#include "simpletools.h"
+
+int main()                                    // Main function
+{
+  print("Hello World");
+}
